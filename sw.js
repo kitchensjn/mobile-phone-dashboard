27116ed3d9c24e97f96e0df59480fcdf52890155
James Kitchens <kitchensjn@gmail.com>
2020-12-04 13:04:04 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
 	e.waitUntil(
 		caches.open("static").then(cache => {
-			return cache.addAll(["./", "./src/master.css", "./images/wheel192.png"]);
+			return cache.addAll(["./", "./src/master.css", "./src/index.js", "./images/wheel192.png"]);
 		})
 	);
 })
